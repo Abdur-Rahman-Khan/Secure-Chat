df3aca4a6088c5fdfe3bfd64c96264f996354774
Abdur-Rahman-Khan <abdurrahman@iitbhilai.ac.in>
2022-11-17 15:57:31 +0530

    database connection

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
-var path = require('path');
+//var database = require('./database');
+var mongo = require('mongodb');
+const mongoose = require("mongoose");
 
 // Define our application
 const app = express();
@@ -7,38 +9,23 @@ const app = express();
 // Set 'port' value to either an environment value PORT or 3000
 app.set('port', process.env.PORT || 3000);
 
-
-// Create router
-//var router = require('./router');
-
-
 // Router listens on / (root)
-//app.use('/', router);
-app.use(express.static('app'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/app/views/'+'index.html'));
-})
+var route = require('./router');
+app.use('/', route);
 
 
-app.get('/register',function(req,res){
-    res.sendFile(path.join(__dirname+'/app/views/'+'register.html'));
-})
+const configFile = require('./myUrl.js');
+const db = configFile.mongoURL+configFile.userName+":"+configFile.password+configFile.restUrl;
 
-app.get('/login',function(req,res){
-    res.sendFile(path.join(__dirname+'/app/views/'+'login.html'));
-})
 
-app.get('/reset',function(req,res){
-    res.sendFile(path.join(__dirname+'/app/views/'+'reset.html'));
-})
+mongoose
+  .connect(db)
+  .then(()=>console.log("Connected Successfully"))
+  .catch(err=>console.log(err));
 
-app.get('/chatbox',function(req,res){
-    res.sendFile(path.join(__dirname+'/app/views/'+'chatbox.html'));
-})
+    app.listen(app.get('port'), function(){
+        console.log("Express server listening on port " + app.get('port'));
+        console.log("You application is running. You should be able to connect to it on http://localhost:" + app.get('port') );
+    });
 
-// Connect to the database, then execute the "callback" function (in this case an anonymous function which starts listening for requests).
-    // Start listening for requests on the port defined earlier
-app.listen(app.get('port'), function(){
-    console.log("Express server listening on port " + app.get('port'));
-    console.log("You application is running. You should be able to connect to it on http://localhost:" + app.get('port') );
-});
+
---
+++ myUrl.js
@@ -0,0 +1,11 @@
+module.exports = {
+    mongoURL: "mongodb+srv://",
+    userName:"testUser",
+    password:"hackOntheWayCapstone",
+    restUrl:"@cluster0.mwzax.mongodb.net/<dbname>?retryWrites=true&w=majority"
+  };
+  
+
+  //format
+  //restUrl:"mongodb+srv://abdur:hello@123todatabase@cluster0.mwzax.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    
